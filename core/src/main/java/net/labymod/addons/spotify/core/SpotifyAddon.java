0a2825bb744c1a0d0a7aa60f86d530a7bdc2f93c
Nico <jumpingpxl@gmail.com>
2023-01-24 14:46:36 +0100

    update to the latest api, apply new labygradle structure

--- core/src/main/java/net/labymod/addons/spotify/core/SpotifyAddon.java
+++ core/src/main/java/net/labymod/addons/spotify/core/SpotifyAddon.java
@@ -16,13 +16,11 @@
 
 package net.labymod.addons.spotify.core;
 
-import com.google.inject.Singleton;
 import net.labymod.addons.spotify.core.hudwidgets.SpotifyTrackHudWidget;
 import net.labymod.api.addon.LabyAddon;
-import net.labymod.api.models.addon.annotation.AddonListener;
+import net.labymod.api.models.addon.annotation.AddonMain;
 
-@Singleton
-@AddonListener
+@AddonMain
 public class SpotifyAddon extends LabyAddon<SpotifyConfiguration> {
 
   @Override
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,26 @@
 rootProject.name = "spotify-addon"
 
+pluginManagement {
+    val labyGradlePluginVersion = "0.3.7"
+    plugins {
+        id("net.labymod.gradle") version (labyGradlePluginVersion)
+    }
+
+    buildscript {
+        repositories {
+            maven("https://dist.labymod.net/api/v1/maven/release/")
+            maven("https://repo.spongepowered.org/repository/maven-public")
+            mavenCentral()
+            mavenLocal()
+        }
+
+        dependencies {
+            classpath("net.labymod.gradle", "addon", labyGradlePluginVersion)
+        }
+    }
+}
+
+plugins.apply("net.labymod.gradle")
+
 include(":api")
 include(":core")
-include(":versions:v1_8")
-include(":versions:v1_17")
-include(":versions:v1_18")
-include(":versions:v1_19")
