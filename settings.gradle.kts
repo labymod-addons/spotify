274cd5fdbfb2f1f6fbebb5d3081ed442375317b1
Christian <r0bby@gmx.net>
2023-05-26 19:56:41 +0200

    Update api

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "spotify-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.11"
+    val labyGradlePluginVersion = "0.3.22"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
