867d0271fd7a32452325d398fda3d7c0b53c0977
Nico <jumpingpxl@gmail.com>
2024-05-15 11:05:12 +0200

    update to java 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "spotify-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.40"
+    val labyGradlePluginVersion = "0.3.50"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
