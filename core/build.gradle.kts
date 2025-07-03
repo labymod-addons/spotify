ed78037ebfeacdbb5ae0a32ab01be2bcc10b8b16
LabyStudio <labystudio@gmail.com>
2025-07-03 14:23:30 +0200

    update java-spotify-api dependency to version 1.4.2

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     labyProcessor()
     api(project(":api"))
 
-    addonMavenDependency("com.github.LabyStudio:java-spotify-api:1.4.1") {
+    addonMavenDependency("com.github.LabyStudio:java-spotify-api:1.4.2") {
         exclude("com.google.code.gson")
         exclude("net.java.dev.jna")
     }
